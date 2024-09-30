18b4bf261147ee46c2e84da1d3080ad9a69dc3f9
Henry Deutsch <henrymdeutsch@gmail.com>
2024-09-29 21:04:39 -0400

    fix(board): fixed bug and changed props a lil

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Board from "./components/Board";
 function App() {
   const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
   const [textareaContent, setTextareaContent] = useState('');
+  const pgn = "1. h4 a5 2. h5 a4 3. b4 axb3 4. h6 b2 5. hxg7 bxa1=Q 6. gxh8=Q Qxa2 7. d3 Nc6 8. Nd2 Nb4 9. Ndf3 Qa3 10. Qxg8 Qa2 11. Rxh7 Qa3 12. Qxf7# ";
 
   const handleTextareaChange = (event) => {
     setTextareaContent(event.target.value);
@@ -16,7 +17,7 @@ function App() {
     <>
       <div className="w-full h-[100vh] flex justify-center items-center">
         <div className="w-[600px]">
-          <Board PgnTree={{}}/>
+          <Board pgn={pgn}/>
         </div>
         <textarea 
           value={textareaContent}
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
+import PropTypes from 'prop-types';
 
-const Board = ({ PgnTree }) => {
-  const [currFen, setCurrFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+
+const Board = ({ pgn }) => {
+
+
   const chessRef = useRef(new Chess());
-  const currentIndexRef = useRef(-1);
   const movesRef = useRef([]);
+  const currentIndexRef = useRef(-1);
+  const [currFen, setCurrFen] = useState('');
 
+  // Load game history, set key tracking
   const initializeGame = useCallback(() => {
-    const pgn = "1. h4 a5 2. h5 a4 3. b4 axb3 4. h6 b2 5. hxg7 bxa1=Q 6. gxh8=Q Qxa2 7. d3 Nc6 8. Nd2 Nb4 9. Ndf3 Qa3 10. Qxg8 Qa2 11. Rxh7 Qa3 12. Qxf7# ";
     chessRef.current.loadPgn(pgn);
     movesRef.current = chessRef.current.history();
-    console.log('initializing game', chessRef.current.history());
     chessRef.current.reset();
     setCurrFen(chessRef.current.fen());
-  }, []);
+    console.log('setting chess position to', chessRef.current.fen());
+  }, [pgn]);
 
   const handleKeyDown = useCallback((event) => {
     if (event.key === 'ArrowRight' && currentIndexRef.current < movesRef.current.length - 1) {
@@ -45,6 +49,10 @@ const Board = ({ PgnTree }) => {
       position={currFen}
     />
   );
+};
+
+Board.propTypes = {
+  pgn: PropTypes.string.isRequired,
 };
 
 export default Board;
