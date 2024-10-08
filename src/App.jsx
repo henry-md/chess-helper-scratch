3b63ee3463f1b77eafec3a56ca6a3bb16c79ce4b
Henry Deutsch <henrymdeutsch@gmail.com>
2024-10-07 20:09:40 -0400

    added pgn mainline generator to parse text

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,84 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Board from "./components/Board";
-
+// import PgnGenerator from "./utils/PgnGenerator.js";
 
 function App() {
   const [textareaContent, setTextareaContent] = useState('');
-  const pgn = "1. h4 a5 2. h5 a4 3. b4 axb3 4. h6 b2 5. hxg7 bxa1=Q 6. gxh8=Q Qxa2 7. d3 Nc6 8. Nd2 Nb4 9. Ndf3 Qa3 10. Qxg8 Qa2 11. Rxh7 Qa3 12. Qxf7# ";
 
   const handleTextareaChange = (event) => {
     setTextareaContent(event.target.value);
   };
 
+  /* turns a nested pgn into a set of mainline pgns.
+  Ex input:
+  1. e4 e5 
+  ( 1... d5 2. exd5 ) 
+  2. Nf3 Nc6 
+  ( 2... Nf6 3. Nxe5 ) 
+  3. Bb5 a6 4. Ba4 b5 
+  ( 4... Nf6 5. O-O Nxe4 6. Re1 ) 
+  5. Bb3 
+
+  Ex output:
+  [
+    "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 b5 5. Bb3",
+    "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Nxe4 6. Re1",
+    "1. e4 e5 2. Nf3 Nf6 3. Nxe5",
+    "1. e4 d5 2. exd5"
+  ]
+  (the pgns can be returned in any order)
+  */
+  const pgnToMainlines = (pgn) => {
+    const mainlines = [];
+    const moves = pgn.split(/\s+/).filter(token => token.trim() !== '');
+    
+    // returns [line_end_idx, [line,]]
+    const backtrack = (index, currentLine) => {
+      let i = index;
+      let addBack;
+      while (i < moves.length) {
+        const move = moves[i];
+        if (move === '(') {
+          // enter backtrack
+          addBack = currentLine.pop();
+          const [newIndex, sublines] = backtrack(i + 1, currentLine.slice());
+          currentLine.push(addBack);
+          mainlines.push(...sublines);
+          i = newIndex;
+        } else if (move === ')') {
+          // return from backtrack
+          return [i, [currentLine.join(' ')]];
+        } else if (!move.includes('...')) {
+          // It's an actual move
+          currentLine.push(move);
+        }
+        i++;
+      }
+      // Reached end of pgn
+      mainlines.push(currentLine.join(' '));
+      return [i, []];
+    };
+  
+    backtrack(0, []);
+    return mainlines;
+  };
+  
+  // Test the function
+  const testPgn = `1. e4 e5 
+  ( 1... d5 2. exd5 ) 
+  2. Nf3 Nc6 
+  ( 2... Nf6 3. Nxe5 ) 
+  3. Bb5 a6 4. Ba4 b5 
+  ( 4... Nf6 5. O-O Nxe4 6. Re1 ) 
+  5. Bb3`;
+  
+  console.log(pgnToMainlines(testPgn));
+
   return (
     <>
       <div className="w-full h-[100vh] flex justify-center items-center">
         <div className="w-[600px]">
-          <Board pgn={pgn}/>
+          <Board pgn={"1. e4"}/>
         </div>
         <textarea 
           value={textareaContent}
